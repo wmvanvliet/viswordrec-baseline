3520983c941d9a79934a209d5524900b5fb37c54
Marijn van Vliet <w.m.vanvliet@gmail.com>
2022-02-04 11:09:54 +0200

    Update brain plotting to pyvista

--- plot_dipole_locations.py
+++ plot_dipole_locations.py
@@ -13,11 +13,9 @@ data_path = './data'
 
 
 # The brain will be plotted from two angles.
-views = [
-    (180, 90, 440, [0, 0, 0]),
-    (-90, 180, 440, [0, 0, 0]),
-]
-view_names = ['lateral', 'ventral']
+views = ['lateral', 'ventral']
+#map_surface = 'white'
+map_surface = None
 
 ##
 # Read in the dipoles in Talairach coordinates. The resulting positions will be
@@ -25,8 +23,8 @@ view_names = ['lateral', 'ventral']
 foci = []
 subjects = list(range(1, 16))
 for subject in subjects:
-    dips = mne.read_dipole(f'{data_path}/dipoles/sub-{subject:02d}_task-epasana_dipoles_talairach.bdip')
-    dip_selection = pd.read_csv(f'{data_path}/dipoles/sub-{subject:02d}_task-epasana_dipole_selection.tsv', sep='\t', index_col=0)
+    dips = mne.read_dipole(f'{data_path}/dipoles/sub-{subject:02d}/meg/sub-{subject:02d}_task-epasana_dipoles_talairach.bdip')
+    dip_selection = pd.read_csv(f'{data_path}/dipoles/sub-{subject:02d}/meg/sub-{subject:02d}_task-epasana_dipole_selection.tsv', sep='\t', index_col=0)
     pos_tal = dips.pos * 1000
     if 'LeftOcci1' in dip_selection.index:
         foci.append(pos_tal[dip_selection.loc['LeftOcci1'].dipole])
@@ -37,8 +35,6 @@ ga_stc = mne.read_source_estimate(f'{data_path}/grand_average/grand_average-word
 
 
 ## Dipole group 1 (Early visual response)
-fig1 = mlab.figure(1, size=(1000, 1000))
-mne.viz.set_3d_backend('mayavi')
 brain = ga_stc.copy().crop(0.065, 0.11).mean().plot(
     'fsaverage',
     subjects_dir=data_path,
@@ -46,67 +42,63 @@ brain = ga_stc.copy().crop(0.065, 0.11).mean().plot(
     background='white',
     cortex='low_contrast',
     surface='inflated',
-    figure=fig1,
+    clim=dict(kind='value', lims=(3.5, 3.8, 7)),
+    brain_kwargs=dict(show_toolbar=False),
 )
-brain.scale_data_colormap(3.5, 3.8, 7, True)
 
 # Save images without dipoles
-for view, view_name in zip(views, view_names):
-    mlab.view(*view)
-    # Save with a transparent background (must route through mlab.screenshot to obtain this)
-    plt.imsave(f'figures/landmark1_{view_name}_no_dipoles.png', mlab.screenshot(mode='rgba', antialiased=True))
+for view in views:
+    brain.show_view(view)
+    brain.save_image(f'figures/landmark1_{view}_no_dipoles.png')
 
 # Save images with dipoles
-brain.add_foci(foci, map_surface='white', hemi='lh', scale_factor=0.2)
-for view, view_name in zip(views, view_names):
-    mlab.view(*view)
-    plt.imsave(f'figures/landmark1_{view_name}_with_dipoles.png', mlab.screenshot(mode='rgba', antialiased=True))
+brain.add_foci(foci, map_surface=map_surface, hemi='lh', scale_factor=0.2)
+for view in views:
+    brain.show_view(view)
+    brain.save_image(f'figures/landmark1_{view}_with_dipoles.png')
 
 
 ## Dipole group 2 (Letter-string response)
 foci = []
 for subject in subjects:
-    dips = mne.read_dipole(f'{data_path}/dipoles/sub-{subject:02d}_task-epasana_dipoles_talairach.bdip')
-    dip_selection = pd.read_csv(f'{data_path}/dipoles/sub-{subject:02d}_task-epasana_dipole_selection.tsv', sep='\t', index_col=0)
+    dips = mne.read_dipole(f'{data_path}/dipoles/sub-{subject:02d}/meg/sub-{subject:02d}_task-epasana_dipoles_talairach.bdip')
+    dip_selection = pd.read_csv(f'{data_path}/dipoles/sub-{subject:02d}/meg/sub-{subject:02d}_task-epasana_dipole_selection.tsv', sep='\t', index_col=0)
     pos_tal = dips.pos * 1000
     if 'LeftOcciTemp2' in dip_selection.index:
         foci.append(pos_tal[dip_selection.loc['LeftOcciTemp2'].dipole])
 foci = np.array(foci)
-fig2 = mlab.figure(2, size=(1000, 1000))
 brain = ga_stc.copy().crop(0.14, 0.2).mean().plot(
     'fsaverage',
-    subjects_dir=fdata_path,
+    subjects_dir=data_path,
     hemi='lh',
     background='white',
     cortex='low_contrast',
     surface='inflated',
-    figure=fig2,
+    clim=dict(kind='value', lims=(3.5, 3.8, 7)),
+    brain_kwargs=dict(show_toolbar=False),
 )
-brain.scale_data_colormap(3.5, 3.8, 7, True)
 
 # Save images without dipoles
-for view, view_name in zip(views, view_names):
-    mlab.view(*view)
-    # Save with a transparent background (must route through mlab.screenshot to obtain this)
-    plt.imsave(f'figures/landmark2_{view_name}_no_dipoles.png', mlab.screenshot(mode='rgba', antialiased=True))
+for view in views:
+    brain.show_view(view)
+    brain.save_image(f'figures/landmark2_{view}_no_dipoles.png')
 
 # Save images with dipoles
-brain.add_foci(foci, map_surface='white', hemi='lh', scale_factor=0.2)
-for view, view_name in zip(views, view_names):
-    mlab.view(*view)
-    plt.imsave(f'figures/landmark2_{view_name}_with_dipoles.png', mlab.screenshot(mode='rgba', antialiased=True))
+brain.add_foci(foci, map_surface=map_surface, hemi='lh', scale_factor=0.2)
+for view in views:
+    brain.show_view(view)
+    brain.save_image(f'figures/landmark2_{view}_with_dipoles.png')
 
 
 ## Dipole group 3 (N400 response)
 foci = []
 for subject in subjects:
-    dips = mne.read_dipole(f'{data_path}/dipoles/sub-{subject:02d}_task-epasana_dipoles_talairach.bdip')
-    dip_selection = pd.read_csv(f'{data_path}/dipoles/sub-{subject:02d}_task-epasana_dipole_selection.tsv', sep='\t', index_col=0)
+    dips = mne.read_dipole(f'{data_path}/dipoles/sub-{subject:02d}/meg/sub-{subject:02d}_task-epasana_dipoles_talairach.bdip')
+    dip_selection = pd.read_csv(f'{data_path}/dipoles/sub-{subject:02d}/meg/sub-{subject:02d}_task-epasana_dipole_selection.tsv', sep='\t', index_col=0)
     pos_tal = dips.pos * 1000
     if 'LeftTemp3' in dip_selection.index:
         foci.append(pos_tal[dip_selection.loc['LeftTemp3'].dipole])
 foci = np.array(foci)
-fig = mlab.figure(3, size=(1000, 1000))
 brain = ga_stc.copy().crop(0.3, 0.5).mean().plot(
     'fsaverage',
     subjects_dir=data_path,
@@ -114,18 +106,17 @@ brain = ga_stc.copy().crop(0.3, 0.5).mean().plot(
     background='white',
     cortex='low_contrast',
     surface='inflated',
-    figure=fig,
+    clim=dict(kind='value', lims=(3.5, 3.8, 7)),
+    brain_kwargs=dict(show_toolbar=False),
 )
-brain.scale_data_colormap(3.5, 3.8, 7, True)
 
 # Save images without dipoles
-for view, view_name in zip(views, view_names):
-    mlab.view(*view)
-    # Save with a transparent background (must route through mlab.screenshot to obtain this)
-    plt.imsave(f'figures/landmark3_{view_name}_no_dipoles.png', mlab.screenshot(mode='rgba', antialiased=True))
+for view in views:
+    brain.show_view(view)
+    brain.save_image(f'figures/landmark3_{view}_no_dipoles.png')
 
 # Save images with dipoles
-brain.add_foci(foci, map_surface='white', hemi='lh', scale_factor=0.2)
-for view, view_name in zip(views, view_names):
-    mlab.view(*view)
-    plt.imsave(f'figures/landmark3_{view_name}_with_dipoles.png', mlab.screenshot(mode='rgba', antialiased=True))
+brain.add_foci(foci, map_surface=map_surface, hemi='lh', scale_factor=0.2)
+for view in views:
+    brain.show_view(view)
+    brain.save_image(f'figures/landmark3_{view}_with_dipoles.png')
--- plot_model_brain_comparison_mne.py
+++ plot_model_brain_comparison_mne.py
@@ -25,11 +25,9 @@ layer_names = d['layer_names']
 layer_acts = pd.DataFrame(layer_activity.T, columns=layer_names)
 
 # The brain will be plotted from two angles.
-views = [
-    (180, 90, 440, [0, 0, 0]),
-    (-90, 180, 440, [0, 0, 0]),
-]
-view_names = ['lateral', 'ventral']
+views = ['lateral', 'ventral']
+#map_surface = 'white'
+map_surface = None
 
 ##
 # Read in the dipoles and morph their positions to the fsaverage brain.
@@ -40,8 +38,8 @@ foci2 = []
 foci3 = []
 subjects = list(range(1, 16))
 for subject in subjects:
-    dips_tal = mne.read_dipole(f'{data_path}/dipoles/sub-{subject:02d}_task-epasana_dipoles_talairach.bdip')
-    dip_selection = pd.read_csv(f'{data_path}/dipoles/sub-{subject:02d}_task-epasana_dipole_selection.tsv', sep='\t', index_col=0)
+    dips_tal = mne.read_dipole(f'{data_path}/dipoles/sub-{subject:02d}/meg/sub-{subject:02d}_task-epasana_dipoles_talairach.bdip')
+    dip_selection = pd.read_csv(f'{data_path}/dipoles/sub-{subject:02d}/meg/sub-{subject:02d}_task-epasana_dipole_selection.tsv', sep='\t', index_col=0)
     pos_tal = dips_tal.pos * 1000
     if 'LeftOcci1' in dip_selection.index:
         foci1.append(pos_tal[dip_selection.loc['LeftOcci1'].dipole])
@@ -53,7 +51,6 @@ foci1 = np.array(foci1)
 foci2 = np.array(foci2)
 foci3 = np.array(foci3)
 
-mne.viz.set_3d_backend('mayavi')
 os.makedirs('figures', exist_ok=True)
 
 ## Layers 1-5 vs dipole group 1 (Early visual response)
@@ -61,7 +58,6 @@ for layer_name in layer_names[:5]:
     c = mne.read_source_estimate(f'{data_path}/brain_model_comparison/brain_model_comparison_mne_{layer_name}')
     c = c.copy().crop(0.065, 0.115)
     c.data = np.maximum(c.data, 0)
-    fig = mlab.figure(size=(1000, 1000))
     brain = c.plot(
         'fsaverage',
         subjects_dir=data_path,
@@ -69,28 +65,26 @@ for layer_name in layer_names[:5]:
         background='white',
         cortex='low_contrast',
         surface='inflated',
-        figure=fig,
         initial_time = c.get_peak()[1],
+        clim=dict(kind='value', lims=(0.2, 0.55, 0.9)),
     )
-    brain.scale_data_colormap(0.2, 0.55, 0.9, True)
 
     # Save images without dipoles
-    for view, view_name in zip(views, view_names):
-        mlab.view(*view)
-        plt.imsave(f'figures/{layer_name}_{view_name}_mne_no_dipoles.png', mlab.screenshot(mode='rgba', antialiased=True))
+    for view in views:
+        brain.show_view(view)
+        brain.save_image(f'figures/{layer_name}_{view}_mne_no_dipoles.png')
 
     # Save images with dipoles
-    brain.add_foci(foci1, map_surface='white', hemi='lh', scale_factor=0.2)
-    for view, view_name in zip(views, view_names):
-        mlab.view(*view)
-        plt.imsave(f'figures/{layer_name}_{view_name}_mne_with_dipoles.png', mlab.screenshot(mode='rgba', antialiased=True))
+    brain.add_foci(foci1, map_surface=map_surface, hemi='lh', scale_factor=0.2)
+    for view in views:
+        brain.show_view(view)
+        brain.save_image(f'figures/{layer_name}_{view}_mne_with_dipoles.png')
 
 ## Layers 6-7 vs dipole group 2 (Letter string response)
 for layer_name in layer_names[5:7]:
     c = mne.read_source_estimate(f'{data_path}/brain_model_comparison/brain_model_comparison_mne_{layer_name}')
     c = c.copy().crop(0.14, 0.2)
     c.data = np.maximum(c.data, 0)
-    fig = mlab.figure(size=(1000, 1000))
     brain = c.plot(
         'fsaverage',
         subjects_dir=data_path,
@@ -98,28 +92,26 @@ for layer_name in layer_names[5:7]:
         background='white',
         cortex='low_contrast',
         surface='inflated',
-        figure=fig,
         initial_time = c.get_peak()[1],
+        clim=dict(kind='value', lims=(0.1, 0.225, 0.35)),
     )
-    brain.scale_data_colormap(0.1, 0.225, 0.35, True)
 
     # Save images without dipoles
-    for view, view_name in zip(views, view_names):
-        mlab.view(*view)
-        plt.imsave(f'figures/{layer_name}_{view_name}_mne_no_dipoles.png', mlab.screenshot(mode='rgba', antialiased=True))
+    for view in views:
+        brain.show_view(view)
+        brain.save_image(f'figures/{layer_name}_{view}_mne_no_dipoles.png')
 
     # Save images with dipoles
-    brain.add_foci(foci2, map_surface='white', hemi='lh', scale_factor=0.2)
-    for view, view_name in zip(views, view_names):
-        mlab.view(*view)
-        plt.imsave(f'figures/{layer_name}_{view_name}_mne_with_dipoles.png', mlab.screenshot(mode='rgba', antialiased=True))
+    brain.add_foci(foci2, map_surface=map_surface, hemi='lh', scale_factor=0.2)
+    for view in views:
+        brain.show_view(view)
+        brain.save_image(f'figures/{layer_name}_{view}_mne_with_dipoles.png')
 
 ## Layer 8 vs dipole group 3 (N400m response)
 layer_name = layer_names[7]
 c = mne.read_source_estimate(f'{data_path}/brain_model_comparison/brain_model_comparison_mne_{layer_name}')
 c = c.copy().crop(0.3, 0.5)
 c.data = np.maximum(c.data, 0)
-fig = mlab.figure(size=(1000, 1000))
 brain = c.plot(
     'fsaverage',
     subjects_dir=data_path,
@@ -127,18 +119,17 @@ brain = c.plot(
     background='white',
     cortex='low_contrast',
     surface='inflated',
-    figure=fig,
     initial_time = c.get_peak()[1],
+    clim=dict(kind='value', lims=(0.05, 0.125, 0.20)),
 )
-brain.scale_data_colormap(0.05, 0.125, 0.20, True)
 
 # Save images without dipoles
-for view, view_name in zip(views, view_names):
-    mlab.view(*view)
-    plt.imsave(f'figures/{layer_name}_{view_name}_mne_no_dipoles.png', mlab.screenshot(mode='rgba', antialiased=True))
+for view in views:
+    brain.show_view(view)
+    brain.save_image(f'figures/{layer_name}_{view}_mne_no_dipoles.png')
 
 # Save images with dipoles
-brain.add_foci(foci3, map_surface='white', hemi='lh', scale_factor=0.2)
-for view, view_name in zip(views, view_names):
-    mlab.view(*view)
-    plt.imsave(f'figures/{layer_name}_{view_name}_mne_with_dipoles.png', mlab.screenshot(mode='rgba', antialiased=True))
+brain.add_foci(foci3, map_surface=map_surface, hemi='lh', scale_factor=0.2)
+for view in views:
+    brain.show_view(view)
+    brain.save_image(f'figures/{layer_name}_{view}_mne_with_dipoles.png')
